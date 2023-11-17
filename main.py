4789c749363003dd268107fc06cb261538423e56
ArunAvasi <arundavasi@gmail.com>
2023-11-16 21:23:01 -0500

    POST

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, render_template, jsonify
-
-
-
-
+from flask import Flask, request, render_template, jsonify, render_template_string
 
 app = Flask("__main__")
 
 
 @app.route("/")
-def home():
+def index():
+
     return render_template("index.html")
 
+
+@app.route("/submit_number", methods=["POST"])
+def submit_number():
+    number = request.form['number']
+
+    return f"You entered the number: {number}"
 if __name__ == "__main__":
     app.run()
 
