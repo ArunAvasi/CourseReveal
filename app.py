424ad7a1dab0fef9a6a963502dc06db917e3f4f8
ArunAvasi <arundavasi@gmail.com>
2024-02-05 16:17:34 -0500

    User Hash ID

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import firebase_admin
+import firebase_admin.auth as auth
 from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
@@ -24,6 +25,20 @@ def home():  # put application's code here
 def login():  # put application's code here
     return render_template('login.html')
 
+
+@app.route('/verifyToken', methods=['POST'])
+def verify_token():
+    # Extract the ID token from the request.
+    id_token = request.json.get('idToken')
+
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        uid = decoded_token['uid']
+        print(uid)
+
+        return jsonify({'status': 'success', 'uid': uid})
+    except auth.AuthError:
+        return jsonify({'status': 'error', 'message': 'Invalid token'}), 401
 
 if __name__ == '__main__':
     app.run()
